cf76b085f2dc3ab44cd063c93156137729315dfc
Firdavs Boliev <bolievfirdavs0@gmail.com>
2025-09-05 22:38:01 +0500

    all done

--- game/admin.py
+++ game/admin.py
@@ -1,1 +1,8 @@
 from django.contrib import admin
+from .models import Weapon, Monster, GameSession, Character, BattleLog
+
+admin.site.register(Weapon)
+admin.site.register(Monster)
+admin.site.register(GameSession)
+admin.site.register(Character)
+admin.site.register(BattleLog)
